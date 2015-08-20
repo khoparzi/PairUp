ca2b06ef81117b522c97e4ef6ed7d29b997c4605
Jon Hinks <projects.ws@jondh.me.uk>
2015-08-20 18:43:44 +0100

    Report an error if login fails

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,12 @@
 <form method="POST" action="/auth/login">
     {!! csrf_field() !!}
 
+    @if ($errors->count())
+        <p>
+            {{ $errors->first('email') }}
+        </p>
+    @endif
+
     <div>
         Email
         <input type="email" name="email" value="{{ old('email') }}">
