8eeec0b2429bbd1cb6cf77c39ce0ebc6c959479c
Jon Hinks <projects.ws@jondh.me.uk>
2015-10-07 14:46:31 +0100

    Add test skeleton for forgot password func tests

---
+++ tests/ForgotPasswordFunctionalTest.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+/* 
+ * Functional tests for the forgot password screen
+ */
+class ForgotPasswordFunctionalTest extends TestCase
+{
+	/**
+	 * Checks that the user is given an easy, explicit route back to the login page
+	 */
+	public function checkRememberedPasswordAfterAll()
+	{
+		
+	}
+
+	/**
+	 * Checks that the submission of an empty email address results in an error
+	 */
+	public function testSubmitEmptyEmail()
+	{
+		
+	}
+
+	/**
+	 * Test that the submission of an invalid email results in an error
+	 * 
+	 * Valid means "contains exactly one @ symbol, and at least one dot in the domain part"
+	 */
+	public function testInvalidEmail()
+	{
+		
+	}
+
+	/**
+	 * Test that the submission of a valid email results in a success message
+	 * 
+	 * Note there is no distinction between addresses that exist and those that do not.
+	 * 
+	 * In test mode, no actual email should be sent, though we cannot test for this.
+	 */
+	public function testValidEmail()
+	{
+		
+	}
+}
