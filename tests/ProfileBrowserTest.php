9266851c81853eef84a7b0df9466550821a856fb
Jon Hinks <projects.ws@jondh.me.uk>
2015-10-18 18:20:03 +0100

    Swap placeholder tests with suggested test skeletons

--- tests/ProfileBrowserTest.php
+++ tests/ProfileBrowserTest.php
@@ -5,8 +5,71 @@
  */
 class ProfileBrowserTest extends TestCase
 {
-	public function testSomething()
-	{
-		
-	}
+    /**
+     * Tests that a user's details are reflected correctly on a card in the browser
+     */
+    public function testSingleUserCard()
+    {
+
+    }
+
+    /**
+     * Checks that the browser works fine with no users
+     */
+    public function testBrowserNoUsers()
+    {
+
+    }
+
+    /**
+     * Checks that there is no pagination for a low number of users
+     */
+    public function testBrowserNoPagination()
+    {
+
+    }
+
+    /**
+     * Check that there is contiguous page numbers for a few screens of users
+     */
+    public function testBrowserContiguousPagination()
+    {
+
+    }
+
+    /**
+     * Checks that there are pagination number gaps for a large number of users
+     */
+    public function testBrowserGapsInPagination()
+    {
+
+    }
+
+    /**
+     * Checks that clicking on a page gets the right set of profiles
+     */
+    public function testPageNumbers()
+    {
+
+    }
+
+    /**
+     * Checks left pagination arrow appears when it should
+     * 
+     * (It should be hidden for page 1 and not for other pages)
+     */
+    public function testLeftArrow()
+    {
+
+    }
+
+    /**
+     * Checks right pagination arrow appears when it should
+     * 
+     * (It should be hidden for the last page and not for other pages)
+     */
+    public function testRightArrow()
+    {
+
+    }
 }
--- tests/ProfileViewTest.php
+++ tests/ProfileViewTest.php
@@ -5,8 +5,47 @@
  */
 class ProfileViewTest extends TestCase
 {
-	public function testSomething()
-	{
-		
-	}
+    /**
+     * Checks that the username is suitably reflected in the page subheading
+     */
+    public function testProfileTitle()
+    {
+
+    }
+
+    /**
+     * Checks that user information is reflected in the personal info section
+     */
+    public function testProfilePersonalInfo()
+    {
+
+    }
+
+    /**
+     * Checks that an empty skills matrix renders correctly
+     */
+    public function testProfileEmptySkillsMatrix()
+    {
+
+    }
+
+    /**
+     * Checks that skills/stars/flags are correct
+     * 
+     * (Have to decide an ordering here - probably alphabetical on skill name, case-insensitive)
+     */
+    public function testProfileNonEmptySkillsMatrix()
+    {
+
+    }
+
+    /**
+     * Checks that the message box sends an email
+     * 
+     * (I believe Laravel has a feature for this, so an email is not actually sent)
+     */
+    public function testProfileSendEmail()
+    {
+
+    }
 }
