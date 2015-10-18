bf74b04e1bad9e30ffd4a408d0ad5314b9e24b91
Jon Hinks <projects.ws@jondh.me.uk>
2015-10-18 18:08:28 +0100

    Add new empty test files

---
+++ tests/ProfileBrowserTest.php
@@ -0,0 +1,12 @@
+<?php
+
+/* 
+ * Functional tests for the profile browser screen
+ */
+class ProfileBrowserTest extends TestCase
+{
+	public function testSomething()
+	{
+		
+	}
+}
---
+++ tests/ProfileViewTest.php
@@ -0,0 +1,12 @@
+<?php
+
+/* 
+ * Functional tests for viewing the profile page
+ */
+class ProfileViewTest extends TestCase
+{
+	public function testSomething()
+	{
+		
+	}
+}
