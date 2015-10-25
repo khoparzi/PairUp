580167987d349f9c144ae4445f5c67fcba7c448d
Jon Hinks <projects.ws@jondh.me.uk>
2015-10-25 13:21:27 +0000

    Example test implementation

--- tests/RegistrationTest.php
+++ tests/RegistrationTest.php
@@ -15,7 +15,17 @@ class RegistrationTest extends TestCase
 
     public function testPasswordMismatch()
     {
-        // TODO
+        // TODO Set up test user using migration/seeder?
+
+        $this->
+            visit('/register')->
+            type('testuser', 'username')->
+            type('test@example.com', 'email')->
+            type('testpass', 'password1')->
+            type('testpass-different', 'password2')->
+            press('Register')->
+            seePageIs('/register')->
+            see('Passwords do not match, please try again');
     }
 
     public function testUsernameAlreadyTaken()
