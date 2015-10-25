3ab7d229fa5b68587518703948daeb55e0c0ecb1
Jon Hinks <projects.ws@jondh.me.uk>
2015-10-25 12:58:37 +0000

    Start of test skeletons for Naveed

---
+++ tests/RegistrationTest.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Functional tests for the registration screen
+ */
+class RegistrationTest extends TestCase
+{
+    /**
+     * Checks that we get an error under all fields that are empty
+     */
+    public function testAllFieldsMissing()
+    {
+        // TODO
+    }
+
+    public function testPasswordMismatch()
+    {
+        // TODO
+    }
+
+    public function testUsernameAlreadyTaken()
+    {
+        // TODO
+    }
+
+    // TODO more tests go here
+}
