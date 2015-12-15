2fdcd79eeefdd8dd903ae5cc5e3e6925ad4338e2
Jon Hinks <projects.ws@jondh.me.uk>
2015-12-15 19:11:45 +0000

    Add some empty test classes

---
+++ tests/WizardPageTwoTest.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Functional tests for the profile wizard
+ */
+class WizardPageTwoTest extends TestCase
+{
+    public function testDummy()
+    {
+        // @todo This test can be deleted - just here to prevent warnings from PhpUnit
+    }
+}
