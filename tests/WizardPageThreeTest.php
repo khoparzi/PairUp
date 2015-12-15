23a081b5d23dff0be9370d02a8dabb5c825be3a8
Jon Hinks <projects.ws@jondh.me.uk>
2015-12-15 19:19:02 +0000

    Suggested test skeletons for page 3 of the profile wizard

--- tests/WizardPageThreeTest.php
+++ tests/WizardPageThreeTest.php
@@ -5,8 +5,51 @@
  */
 class WizardPageThreeTest extends TestCase
 {
-    public function testDummy()
+    /**
+     * Users may wish not to add a profile, or to add one later
+     */
+    public function testEmptyProfileUrlIsAllowed()
     {
-        // @todo This test can be deleted - just here to prevent warnings from PhpUnit
+        
+    }
+
+    /**
+     * If there is anything in the URL field it must be http or https
+     */
+    public function testNonWebUrlIsDisallowed()
+    {
+        
+    }
+
+    /**
+     * Let's refuse URLs that are too long
+     */
+    public function testExcessivelyLongUrlIsDisallowed()
+    {
+        
+    }
+
+    /**
+     * The description is presently not mandatory
+     */
+    public function testEmptyDescriptionIsAllowed()
+    {
+        
+    }
+
+    /**
+     * Disallow a profile description over a certain length
+     */
+    public function testExcessivelyLongDescriptionIsDisallowed()
+    {
+        
+    }
+
+    /**
+     * Let's check that JavaScript inside Markdown is rendered harmless
+     */
+    public function testJavaScriptIsMadeSafe()
+    {
+        
     }
 }
