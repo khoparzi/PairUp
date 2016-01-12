b742155459243edc08453fc6ad6aa3e320e67d48
umbert <umbertix@gmail.com>
2016-01-12 19:36:23 +0000

    Implemented test for the second page of the wizard.

---
+++ resources/lang/en/wizard.php
@@ -0,0 +1,13 @@
+<?php
+return [
+  "skills_table_columns" => [
+      "skill" => "Skills",
+      "rating" =>"Rating",
+      "help_offer" => "Offering help?",
+      "help_seeking" => "Seeking help?",
+  ],
+    "general" => [
+        "new_skill" => "Add a skill",
+        "no_skills" => "No skills added",
+    ]
+];
--- tests/WizardPageTwoTest.php
+++ tests/WizardPageTwoTest.php
@@ -5,8 +5,66 @@
  */
 class WizardPageTwoTest extends TestCase
 {
-    public function testDummy()
+    public function testHyperLinkToAddANewSkillExist()
     {
-        // @todo This test can be deleted - just here to prevent warnings from PhpUnit
+        $this
+            ->visit('/login')
+            ->type('valid@testEmail.com', 'email')
+            ->pressSubmit()
+            ->visit('/edit/profile/2')
+            ->see(trans("wizard.general.new_skill"));
+    }
+
+    public function testPreviousButtonExist()
+    {
+        $this
+            ->visit('/login')
+            ->type('valid@testEmail.com', 'email')
+            ->pressSubmit()
+            ->visit('/edit/profile/2')
+            ->see(trans("pagination.previous"));
+    }
+
+    public function testNextButtonExist()
+    {
+        $this
+            ->visit('/login')
+            ->type('valid@testEmail.com', 'email')
+            ->pressSubmit()
+            ->visit('/edit/profile/2')
+            ->see(trans("pagination.next"));
+    }
+
+    public function testPreviousButtonRedirect()
+    {
+        $this
+            ->visit('/login')
+            ->type('valid@testEmail.com', 'email')
+            ->pressSubmit()
+            ->visit('/edit/profile/2')
+            ->press(trans("pagination.previous"))
+            ->seePageIs(route('wizard.FirstPage'));
+    }
+
+    public function testNextButtonRedirect()
+    {
+        $this
+            ->visit('/login')
+            ->type('valid@testEmail.com', 'email')
+            ->pressSubmit()
+            ->visit('/edit/profile/2')
+            ->press(trans("pagination.next"))
+            ->seePageIs(route('wizard.ThirdPage'));
+    }
+
+    public function testNewSkillButtonRedirect()
+    {
+        $this
+            ->visit('/login')
+            ->type('valid@testEmail.com', 'email')
+            ->pressSubmit()
+            ->visit('/edit/profile/2')
+            ->press(trans("wizard.general.new_skill"))
+            ->seePageIs(route('wizard.AddSkill'));
     }
 }
