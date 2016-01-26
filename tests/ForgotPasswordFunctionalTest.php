605cbf8ed36d7451574caadf95f95a587a60751d
Jon Hinks <projects.ws@jondh.me.uk>
2016-01-26 18:29:11 +0000

    Swap out hardwired home page for named route

--- tests/ForgotPasswordFunctionalTest.php
+++ tests/ForgotPasswordFunctionalTest.php
@@ -16,7 +16,7 @@ class ForgotPasswordFunctionalTest extends TestCase
         $this
             ->loginTestUser()
             ->visitForgotPasswordPage()
-            ->seePageIs('/')
+            ->seePageIs(route('public.welcome'))
             ->see(trans('public.words.forgotten.signOutFirst'));
     }
 
