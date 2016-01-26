b750b025613d1cf794f5832785ce130a4f80ad23
Jon Hinks <projects.ws@jondh.me.uk>
2016-01-26 18:46:51 +0000

    Add dummy view for the edit profile page

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,20 @@ Route::get('/', ["as" => "public.welcome" , function () {
     return view('welcome');
 }]);
 
+// @todo Make {page} optional and use a regex match
+
+// Edit profile (first page)
+Route::get(
+    'edit/profile',
+    ["as" => "profile.edit", 'uses' => 'Profile\ProfileController@editFirstPage']
+);
+
+// Edit profile by page number
+Route::get(
+    'edit/profile/{page}',
+    ["as" => "profile.edit", 'uses' => 'Profile\ProfileController@editFirstPage']
+);
+
 // Authentication routes
 Route::get('auth/login', 'Auth\AuthController@getLogin');
 Route::post('auth/login', 'Auth\AuthController@postLogin');
